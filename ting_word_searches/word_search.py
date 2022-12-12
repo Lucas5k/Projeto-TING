85c2e5637e995d60968e04dca041b5d4891944c7
Lucas5k <lucascabral112347@gmail.com>
2022-12-12 18:47:25 -0300

    feat: corrigindo lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -12,7 +12,9 @@ def exists_word(word, queue: Queue):
 			"palavra": word,
 			"arquivo": search_word["nome_do_arquivo"],
 			"ocorrencias": [
-				{ "linha": ind + 1 }
+				{
+					"linha": ind + 1
+				}
 				for ind, row in enumerate(search_word["linhas_do_arquivo"])
 				if word in row.lower()
 			]
