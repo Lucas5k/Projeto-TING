5d4e708e01d3d677be52ad05a13e839530770012
Lucas5k <lucascabral112347@gmail.com>
2022-12-12 18:45:10 -0300

    feat: corrigindo lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,6 +3,7 @@ from ting_file_management.queue import Queue
 
 def exists_word(word, queue: Queue):
 		"""Aqui irá sua implementação"""
+
 		list_of_exists_word = list()
 		for position in range(len(queue._queue)):
 			search_word = queue.search(position)
@@ -11,9 +12,7 @@ def exists_word(word, queue: Queue):
 				"palavra": word,
 				"arquivo": search_word["nome_do_arquivo"],
 				"ocorrencias": [
-					{
-						"linha": ind + 1
-					}
+					{ "linha": ind + 1 }
 					for ind, row in enumerate(search_word["linhas_do_arquivo"])
 					if word in row.lower()
 				]
@@ -28,3 +27,4 @@ def exists_word(word, queue: Queue):
 
 def search_by_word(word, queue: Queue):
 		"""Aqui irá sua implementação"""
+
