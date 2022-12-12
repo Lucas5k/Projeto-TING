94be545f9ff5e61a29183014b58ee01b12b339e9
Lucas5k <lucascabral112347@gmail.com>
2022-12-12 17:33:21 -0300

    feat: corrigindo lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,6 +2,7 @@ import sys
 from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
+
 def process(path_file, queue: Queue):
     """Aqui irá sua implementação"""
     if path_file in str(queue._queue):
@@ -18,7 +19,6 @@ def process(path_file, queue: Queue):
     print(file_of_dicts, file=sys.stdout)
 
 
-
 def remove(queue: Queue):
     """Aqui irá sua implementação"""
     if not len(queue._queue):
@@ -30,16 +30,10 @@ def remove(queue: Queue):
     print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
-
 def file_metadata(queue: Queue, position):
     """Aqui irá sua implementação"""
     try:
         result = queue.search(position)
         print(f"{result}", file=sys.stdout)
-    except:
+    except IndexError:
         print(f"Posição inválida", file=sys.stderr)
-
-
-if __name__ == '__main__':
-	Pro = process("statics/arquivo_teste.txt", Queue)
-	print(Pro)
