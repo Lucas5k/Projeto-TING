19f79395909c601c6aea780dd520c8ebfe0686d7
Lucas5k <lucascabral112347@gmail.com>
2022-12-12 18:42:25 -0300

    feat: corrigindo lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -4,7 +4,6 @@ from ting_file_management.queue import Queue
 def exists_word(word, queue: Queue):
 		"""Aqui irá sua implementação"""
 		list_of_exists_word = list()
-
 		for position in range(len(queue._queue)):
 			search_word = queue.search(position)
 
